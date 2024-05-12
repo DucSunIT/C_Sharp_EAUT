edb3490856606b65360aee6edc2f67d09a0df4ae
ducdev <151699432+DucSunIT@users.noreply.github.com>
2024-05-13 00:20:27 +0700

    update_lesson

--- WindowsForm/Bai2/Form1.cs
+++ WindowsForm/Bai2/Form1.cs
@@ -34,58 +34,105 @@ namespace Bai3
 
         private void button4_Click(object sender, EventArgs e)
         {
-            Close(); // thoát khỏi chương trình 
+            DialogResult result = MessageBox.Show(
+                "Bạn có chắc chắn muốn thoát",
+                "Confirm Exit",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+            if(result == DialogResult.Yes)
+            {
+                Close(); // thoát khỏi chương trình 
+            }
         }
 
         private void btnTong_Click(object sender, EventArgs e)
         {
-            long tong=0;
-            // tetxt -> biết được người dùng nhập gì vào 
-            tong = long.Parse(txtA.Text) + long.Parse(txtB.Text);
-            lblKetQua.Text = txtA.Text + " + " + txtB.Text + " = " +  tong + "";
-        }
-
-        private void btnHieu_Click(object sender, EventArgs e)
-        {
-            long hieu =0;
-            hieu = long.Parse(txtA.Text) - long.Parse(txtB.Text);
-            lblKetQua.Text = txtA.Text + " - " + txtB.Text + " = " + hieu + "";
-        }
-
-        private void btnReset_Click(object sender, EventArgs e)
-        {
-            lblKetQua.Text = "Coder by AlexDuc";
-            txtA.Text = "";
-            txtB.Text = "";
-        }
-
-        private void btnNhan_Click(object sender, EventArgs e)
-        {
-            long nhan=1;
-            if(long.Parse(txtA.Text)==0 || long.Parse(txtB.Text) == 0)
+            try
             {
-                lblKetQua.Text = "0 nhân cái gì cũng bằng 0 thôi ku !!";
-            }
-            else
+                long tong = 0;
+                // tetxt -> biết được người dùng nhập gì vào 
+                tong = long.Parse(txtA.Text) + long.Parse(txtB.Text);
+                lblKetQua.Text = txtA.Text + " + " + txtB.Text + " = " + tong + "";
+            }catch(Exception ex)
             {
-                nhan = long.Parse(txtA.Text) * long.Parse(txtB.Text);
-                lblKetQua.Text = txtA.Text + " * " + txtB.Text + " = " + nhan + "";
+                MessageBox.Show("Vui lòng nhập giá trị!", "Empty Value");
             }
             
         }
 
+        private void btnHieu_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                long hieu = 0;
+                hieu = long.Parse(txtA.Text) - long.Parse(txtB.Text);
+                lblKetQua.Text = txtA.Text + " - " + txtB.Text + " = " + hieu + "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Vui lòng nhập giá trị!", "Empty Value");
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                "Bạn có chắc chắn muốn reset", 
+                "Confirm Reset",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+            if(result == DialogResult.Yes)
+            {
+                lblKetQua.Text = "Coder by AlexDuc";
+                txtA.Text = "";
+                txtB.Text = "";
+            }
+        }
+
+        private void btnNhan_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                long nhan = 1;
+                if (long.Parse(txtA.Text) == 0 || long.Parse(txtB.Text) == 0)
+                {
+                    lblKetQua.Text = "0 nhân cái gì cũng bằng 0 thôi ku !!";
+                }
+                else
+                {
+                    nhan = long.Parse(txtA.Text) * long.Parse(txtB.Text);
+                    lblKetQua.Text = txtA.Text + " * " + txtB.Text + " = " + nhan + "";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Vui lòng nhập giá trị!", "Empty Value");
+            }
+
+
+        }
+
         private void btnChia_Click(object sender, EventArgs e)
         {
-            double chia;
-            if (double.Parse(txtB.Text) == 0)
+            try
             {
-                lblKetQua.Text = "Không chia được cho số 0 nha ku !";
-            }else
-            {
-                chia = double.Parse(txtA.Text) / double.Parse(txtB.Text);
-                lblKetQua.Text = txtA.Text + " / " + txtB.Text + " = " + chia + "";
+                double chia;
+                if (double.Parse(txtB.Text) == 0)
+                {
+                    lblKetQua.Text = "Không chia được cho số 0 nha ku !";
+                }
+                else
+                {
+                    chia = double.Parse(txtA.Text) / double.Parse(txtB.Text);
+                    lblKetQua.Text = txtA.Text + " / " + txtB.Text + " = " + chia + "";
+                }
             }
-           
+            catch (Exception ex)
+            {
+                MessageBox.Show("Vui lòng nhập giá trị!", "Empty Value");
+            }
+
+
         }
     }
 }
---
+++ WindowsForm/Bai3/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Bai3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDV = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDV
+            // 
+            this.btnDV.BackColor = System.Drawing.Color.Red;
+            this.btnDV.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDV.ForeColor = System.Drawing.Color.Yellow;
+            this.btnDV.Location = new System.Drawing.Point(271, 182);
+            this.btnDV.Name = "btnDV";
+            this.btnDV.Size = new System.Drawing.Size(291, 66);
+            this.btnDV.TabIndex = 0;
+            this.btnDV.Text = "Download Virus";
+            this.btnDV.UseVisualStyleBackColor = false;
+            this.btnDV.Click += new System.EventHandler(this.btnDV_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(379, 309);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(183, 76);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDV);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDV;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
