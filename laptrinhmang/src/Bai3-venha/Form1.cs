6617e121a77e63ca0905f2701c060552dacde7c4
ducdev <151699432+DucSunIT@users.noreply.github.com>
2024-05-17 16:01:56 +0700

    update

---
+++ laptrinhmang/src/Bai3-venha/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace Bai3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReadFile_Click(object sender, EventArgs e)
+        {
+            string fileName = txtFIileName.Text + ".txt";
+            string contentFile = txtContentFile.Text;
+            File.WriteAllText(fileName, contentFile);
+            MessageBox.Show("Bạn đã ghi file thành công");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string docFile = File.ReadAllText(txtFIileName.Text + ".txt");
+            txtContentFile.Text = docFile;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                "Bạn có muốn thoát",
+                "Exit",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+            if(result==DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
---
+++ lythuyet/OOP/Bai2-venha/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2_venha
+{
+    class NhanVien
+    {
+        string hoTen; // null
+        int tuoi; // 0
+        string diaChi;
+        double luongCoBan;
+        double heSo;
+        double phuCap;
+        
+
+        public double tienLuong()
+        {
+            return luongCoBan * heSo + phuCap;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            NhanVien nv = new NhanVien();
+            Console.WriteLine("Nhập họ tên : ");
+            nv.hoTen = Console.ReadLine();
+            Console.WriteLine("Nhập tuổi : ");
+            nv.tuoi = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập địa chỉ: ");
+            nv.diaChi = Console.ReadLine();
+            Console.WriteLine("Nhập lương cơ bản: ");
+            nv.luongCoBan = double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập hệ số: ");
+            nv.heSo = double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập phụ cấp: ");
+            nv.phuCap = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Tiền lương: " + nv.tienLuong() +" tr");
+            Console.ReadLine();
+        }
+    }
+}
